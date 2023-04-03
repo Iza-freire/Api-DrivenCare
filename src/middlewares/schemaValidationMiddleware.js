9938564002196b4b164c5c1fe0cc90fe7d5e83a0
IzaFreire <kariny.frei.2000@gmail.com>
2023-04-03 12:07:11 -0300

    feat: config schema and midlleware

--- src/middlewares/schemaValidationMiddleware.js
+++ src/middlewares/schemaValidationMiddleware.js
@@ -1,1 +1,12 @@
 
+export function validateSchemma(schema) {
+    return (req, res, next) => {
+        const { error } = schema.validate(req.body, { abortEarly: false });
+        if (error) {
+            return res
+                .status(422)
+                .send(error.details.map((detail) => detail.message));
+        }
+        next();
+    }
+}
